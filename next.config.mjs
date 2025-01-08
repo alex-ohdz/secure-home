92a09c65f1a466f9c302a938cbeb8cdd21211619
alex-ohdz <alejandrooramash@gmail.com>
2025-01-09 00:25:17 +0100

    despliegue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+	output: 'export', // Configuración para exportar estáticos
+	basePath: '/camaraiot', // Ajusta esto al nombre de tu repositorio en GitHub
+	assetPrefix: '/camaraiot/', // Prefijo para activos estáticos
+  };
+  
+  export default nextConfig;
+  
